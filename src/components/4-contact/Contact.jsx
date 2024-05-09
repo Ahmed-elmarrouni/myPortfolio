030ce773f2534b44bac4ebcb837e0f3737705505
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2024-05-09 12:33:15 +0100

    Added Contact section to portfolioio

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -3,9 +3,38 @@ import './contact.css';
 
 function Contact() {
   return (
-    <div>
-      Contact
-    </div>
+    <>
+
+      <section className='contact-me'>
+        <h1 className='title'>
+          <span className='icon-envelope'></span>
+          Contact me
+        </h1>
+
+        <p className='subtitle'>Contact me for more information </p>
+
+        <div className='flex'>
+          <form className=''>
+            <div>
+              <label htmlFor='email' >Email Address :</label>
+              <input required type="email" placeholder="Email Address" id='email' />
+            </div>
+
+            <div className='flex' style={{ marginTop: "24px" }}>
+              <label htmlFor="message" >Your Message :</label>
+              <textarea required id='message' placeholder='message' ></textarea>
+            </div>
+
+            <button className='send'>
+              Send 
+              <span className='icon-send' style={{marginLeft:"35px"}}></span></button>
+          </form>
+          <div className='animation '>
+            animation</div>
+        </div>
+      </section>
+
+    </>
   )
 }
 
