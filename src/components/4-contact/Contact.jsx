24bee42422c7695728b03a08285bdbb9cfa581c5
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2024-05-14 16:00:27 +0100

    Update Contact Section : use Emailjs

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,41 +1,82 @@
-
 import './contact.css';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const [formStatus, setFormStatus] = useState('Send Message');
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_3en7llb', 'template_4renhmb', e.target, {
+        publicKey: 'rHIOrNK_gaf0FWLgF',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setFormStatus('Sent');
+          setFormData({
+            name: '',
+            email: '',
+            message: ''
+          });
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setFormStatus('Error');
+        }
+      );
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
   return (
     <>
-
       <section className='contact-me'>
         <h1 className='title'>
           <span className='icon-envelope'></span>
           Contact me
         </h1>
-
-        <p className='subtitle'>Contact me for more information </p>
-
+        <p className='subtitle'>Contact me for more information</p>
         <div className='flex'>
-          <form className=''>
+          <form onSubmit={sendEmail} className='' method='Post'>
             <div>
-              <label htmlFor='email' >Email Address :</label>
-              <input required type="email" placeholder="Email Address" id='email' />
+              <label htmlFor='fullName'>Full &nbsp; Name  &nbsp; &nbsp; &nbsp;:</label>
+              <input required type="text" placeholder="Full Name" name="name" id='fullName' value={formData.name} onChange={handleInputChange} />
             </div>
-
+            <br />
+            <div>
+              <label htmlFor='email'>Email Address :</label>
+              <input required type="email" placeholder="Email Address" name="email" id='email' value={formData.email} onChange={handleInputChange} />
+            </div>
             <div className='flex' style={{ marginTop: "24px" }}>
-              <label htmlFor="message" >Your Message :</label>
-              <textarea required id='message' placeholder='message' ></textarea>
+              <label htmlFor="message">Your Message :</label>
+              <textarea required id='message' placeholder='Message' name="message" value={formData.message} onChange={handleInputChange}></textarea>
             </div>
-
-            <button className='send'>
-              Send 
-              <span className='icon-send' style={{marginLeft:"35px"}}></span></button>
+            <button className='send' type="submit">
+              {formStatus}
+              <span className='icon-send' style={{ marginLeft: "35px" }}></span>
+            </button>
           </form>
-          <div className='animation '>
-            animation</div>
+          <div className='animation'>
+            animation
+          </div>
         </div>
       </section>
-
     </>
   )
 }
 
-export default Contact
+export default Contact;
