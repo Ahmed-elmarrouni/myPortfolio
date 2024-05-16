cd6a6aebd537964ebee0bdba3ca5efc7b0032ea2
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2024-05-16 13:25:46 +0100

    Apply motion effect to study cards

--- src/components/3-study/Study.jsx
+++ src/components/3-study/Study.jsx
@@ -1,26 +1,62 @@
 import './study.css';
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function Study() {
+
+    const variants = {
+        visible: { opacity: 1, y: 0 },
+        hidden: { opacity: 0, y: 100 }, 
+    };
+
+    useInView({
+        triggerOnce: true,
+        threshold: 0.5, 
+    });
+
+    const { ref: toolsInViewRef, inView: toolsInView } = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+
+
+
+
     return (
         <>
             <h1 className='education'>Education</h1>
             <div className="vertical-line">
                 <div className="circle top"></div>
-                <div className="left">
+                <motion.div
+                    ref={toolsInViewRef}
+                    layout
+                    initial="hidden"
+                    animate={toolsInView ? "visible" : "hidden"}
+                    variants={variants}
+                    transition={{ type: "spring", damping: 10, stiffness: 100, delay: 0.2 }}
+                    className="left">
                     <h1 className='title'>Specialized Technician in Full-Stack Development</h1>
                     <h3 className='subtitle'>ISTA Tetouan Park - Tetouan, Morocco</h3>
                     <p className='date'>2022 - 2024</p>
-                </div>
+                </motion.div>
                 <div className="circle middle"></div>
-                <div className="right">
+
+
+                <motion.div
+                    ref={toolsInViewRef}
+                    layout
+                    initial="hidden"
+                    animate={toolsInView ? "visible" : "hidden"}
+                    variants={variants}
+                    transition={{ type: "spring", damping: 10, stiffness: 100, delay: 0.2 }}
+
+                    className="right">
                     <h1 className='title'>High School Diploma in Mechanical Technology Science, STM</h1>
                     <h3 className='subtitle'>Imam Al Ghazali Technical High School - Tetouan, Morocco</h3>
                     <p className='date'>2022</p>
-                </div>
+                </motion.div>
             </div>
-
         </>
-
     );
 }
 
