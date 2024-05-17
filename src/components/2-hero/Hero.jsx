27a690e1560519553f1bb8f5f328d3c72688e890
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2024-05-17 15:03:09 +0100

    Edite button to download cv

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -120,10 +120,13 @@ function Hero() {
           </div>
         </div>
 
-
-
         <div className='CvBtn'>
-          <button className="animated-button">
+          <a
+            className="animated-button"
+            href="https://drive.google.com/file/d/17WDYot5t--mvU_hgtetBoB4s0k6rrYOS/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            role="button"          >
             <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
@@ -136,7 +139,8 @@ function Hero() {
                 d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
               ></path>
             </svg>
-          </button>
+          </a>
+
         </div>
 
       </div>
